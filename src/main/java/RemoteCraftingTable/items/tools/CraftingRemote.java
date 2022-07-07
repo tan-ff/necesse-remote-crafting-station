1707522ce60432d6a0aad5e4f75ea22ed9984a00
Tanyrus <unbridledscholar@gmail.com>
2022-07-06 17:11:45 -0700

    Initial Commit for Tooltips

--- src/main/java/RemoteCraftingTable/items/tools/CraftingRemote.java
+++ src/main/java/RemoteCraftingTable/items/tools/CraftingRemote.java
@@ -6,23 +6,26 @@ import necesse.engine.network.PacketReader;
 import necesse.engine.network.packet.PacketSpawnProjectile;
 import necesse.engine.sound.SoundEffect;
 import necesse.engine.util.GameRandom;
+import necesse.entity.mobs.GameDamage;
 import necesse.gfx.GameResources;
 import necesse.gfx.gameTooltips.ListGameTooltips;
 import necesse.inventory.InventoryItem;
 import necesse.inventory.PlayerInventorySlot;
+import necesse.inventory.item.Item;
 import necesse.level.maps.Level;
 import necesse.inventory.item.toolItem.miscToolItem.TestToolItem;
+import necesse.inventory.item.trinketItem.SimpleTrinketItem;
 
 public class CraftingRemote extends TestToolItem {
 
     public CraftingRemote() {
-        rarity = Rarity.RARE;
-
     }
-    public ListGameTooltips getTooltips(TestToolItem item) {
+
+    public ListGameTooltips getTooltip(TestToolItem item) {
         ListGameTooltips tooltips = new ListGameTooltips();
-        tooltips.add(Localization.translate("Test: Adding tooltip for Remote Crafting Tool.", "Test: examplestafftip"));
+        tooltips.add(Localization.translate("itemtooltip", "craftingremote"));
         return tooltips;
+        //return new ListGameTooltips("+20% Movement Speed");
     }
 
 }
